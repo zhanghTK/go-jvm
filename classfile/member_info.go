327e7c8249d8571f3c663cb8490de86b4d4d1ea5
zhanghTK <510223064@qq.com>
2017-07-02 00:07:30 +0800

    feat 启动解析class文件

--- classfile/member_info.go
+++ classfile/member_info.go
@@ -26,7 +26,7 @@ func readMember(reader *ClassReader, cp ConstantPool) *MemberInfo {
 		accessFlags:     reader.readUint16(),
 		nameIndex:       reader.readUint16(),
 		descriptorIndex: reader.readUint16(),
-		//attributes:      readAttributes(reader, cp),
+		attributes:      readAttributes(reader, cp),
 	}
 }
 
