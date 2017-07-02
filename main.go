2e875c673469c978f65b3ef792e35436daf079d5
zhanghTK <510223064@qq.com>
2017-07-02 13:09:51 +0800

    test 测试局部变量表、操作数栈

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"GJvm/classpath"
 	"fmt"
-	"strings"
-	"GJvm/classfile"
+	"GJvm/rtda"
 )
 
 func main() {
@@ -18,44 +16,42 @@ func main() {
 	}
 }
 
-// -Xjre "/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home/jre" java.lang.Object
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class: %s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
-	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
-	fmt.Printf("classpath:%v class:%v args:%v\n", cp, cmd.class, cmd.args)
-	className := strings.Replace(cmd.class, ".", "/", -1)
-	cf := loadClass(className, cp)
-	printClassInfo(cf)
+	frame := rtda.NewFrame(100, 100)
+	testLocalVars(frame.LocalVars())
+	testOperandStack(frame.OperandStack())
 }
 
-func loadClass(className string, cp *classpath.Classpath) *classfile.ClassFile {
-	classData, _, err := cp.ReadClass(className)
-	if err != nil {
-		panic(err)
-	}
-
-	cf, err := classfile.Parse(classData)
-	fmt.Println(classData)
-	if err != nil {
-		panic(err)
-	}
-
-	return cf
+func testLocalVars(vars rtda.LocalVars) {
+	vars.SetInt(0, 100)
+	vars.SetInt(1, -100)
+	vars.SetLong(2, 2997924580)
+	vars.SetLong(4, -2997924580)
+	vars.SetFloat(6, 3.1415926)
+	vars.SetDouble(7, 2.71828182845)
+	vars.SetRef(9, nil)
+	println(vars.GetInt(0))
+	println(vars.GetInt(1))
+	println(vars.GetLong(2))
+	println(vars.GetLong(4))
+	println(vars.GetFloat(6))
+	println(vars.GetDouble(7))
+	println(vars.GetRef(9))
 }
 
-func printClassInfo(cf *classfile.ClassFile) {
-	fmt.Printf("version: %v.%v\n", cf.MajorVersion(), cf.MinorVersion())
-	fmt.Printf("constants count: %v\n", len(cf.ConstantPool()))
-	fmt.Printf("access flags: 0x%x\n", cf.AccessFlags())
-	fmt.Printf("this class: %v\n", cf.ClassName())
-	fmt.Printf("super class: %v\n", cf.SuperClassName())
-	fmt.Printf("interfaces: %v\n", cf.InterfaceNames())
-	fmt.Printf("fields count: %v\n", len(cf.Fields()))
-	for _, f := range cf.Fields() {
-		fmt.Printf("  %s\n", f.Name())
-	}
-	fmt.Printf("methods count: %v\n", len(cf.Methods()))
-	for _, m := range cf.Methods() {
-		fmt.Printf("  %s\n", m.Name())
-	}
+func testOperandStack(ops *rtda.OperandStack) {
+	ops.PushInt(100)
+	ops.PushInt(-100)
+	ops.PushLong(2997924580)
+	ops.PushLong(-2997924580)
+	ops.PushFloat(3.1415926)
+	ops.PushDouble(2.71828182845)
+	ops.PushRef(nil)
+	println(ops.PopRef())
+	println(ops.PopDouble())
+	println(ops.PopFloat())
+	println(ops.PopLong())
+	println(ops.PopLong())
+	println(ops.PopInt())
+	println(ops.PopInt())
 }
